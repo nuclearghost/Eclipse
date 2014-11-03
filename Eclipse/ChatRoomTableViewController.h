1973a454b4013565e1776aadfb0f43df2eca7067
Mark Meyer <mark.meyer37@gmail.com>
2014-11-02 17:19:41 -0800

    Added ChatRoom controller

---
+++ Eclipse/ChatRoomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatRoomTableViewController.h
+//  Eclipse
+//
+//  Created by Mark Meyer on 11/2/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatRoomTableViewController : UITableViewController
+
+@end
