9a7b952fa328e88f3db8a2007a8beee61f0f3ea6
Mark Meyer <mark.meyer37@gmail.com>
2014-12-13 19:51:44 -0800

    Add beforeSave hook for chatRooms

--- ParseCloudCode/cloud/main.js
+++ ParseCloudCode/cloud/main.js
@@ -1,6 +1,11 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.beforeSave("ChatRoom", function(request, response) {
+  var ChatRoom = request.object;
+  var expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 1); //Should come from config
+  request.object.set("expiresAt", expirationDate);
+  request.object.set("active", true);
+  request.object.set("radius", 3.5); //Should come from config
+  response.success();
 });
+
